c3d4caabbaecbf41b13176ed3d12bc49a4942101
HiteshDubey <reenadubey7000@gmail.com>
2026-01-10 12:49:03 +0530

    Implemented migrations

---
+++ models/model.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from Database.base import Base
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer , primary_key=True)
+    name = Column(String , nullable=False)
+    password = Column(String , nullable=False)
+    email = Column(String , nullable=False , unique=True)
+
+    is_verified = Column(Boolean , nullable=False , default=False)
+
+
+
